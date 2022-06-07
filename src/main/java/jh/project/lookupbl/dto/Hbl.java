20f0efc0900ceb1d60c82c538b8bbdf1aa009cf0
pilming44 <pilming44@naver.com>
2022-06-07 23:58:21 +0900

    hbl구분자 입력시 연속 번호 입력 후 저장하는 로직 작업중 이슈)ccik205032,33,34,35 로 했을경우 분리해서 연속으로 붙이는건 되는데 ccik205032~35나 ccik205032-35로 했을경우 연속으로 붙여주는건 안됨

---
+++ src/main/java/jh/project/lookupbl/dto/Hbl.java
@@ -0,0 +1,12 @@
+package jh.project.lookupbl.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Hbl {
+
+    private String hblNo;
+    private String hblYear;
+}
--- src/main/java/jh/project/lookupbl/service/MainService.java
+++ src/main/java/jh/project/lookupbl/service/MainService.java
@@ -1,6 +1,7 @@
 package jh.project.lookupbl.service;
 
 import jh.project.lookupbl.dto.CargCsclPrgs;
+import jh.project.lookupbl.dto.Hbl;
 import jh.project.lookupbl.form.HblForm;
 import jh.project.lookupbl.xmlObject.CargCsclPrgsInfoQryRtnVoTag;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +15,8 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MainService {
@@ -57,5 +60,47 @@ public class MainService {
         CargCsclPrgsInfoQryRtnVoTag cargCsclPrgsInfoQryRtnVoTag = (CargCsclPrgsInfoQryRtnVoTag) unmarshaller.unmarshal(doc);
 
         return cargCsclPrgsInfoQryRtnVoTag;
+    }
+
+    public List<Hbl> extractHblNo(HblForm form) {
+        List<Hbl> hblList = new ArrayList<>();
+
+        //공백제거
+        form.setHblNo(form.getHblNo().replaceAll(" ", ""));
+        form.setHblNo(form.getHblNo().trim());
+
+        //구문문자체크
+        String[] checkSplit = form.getHblNo().split(",|/|-|~");
+
+        //구분문자가있을경우
+        if(checkSplit.length > 1) {
+            for(int i =0; i < checkSplit.length; i++) {
+                Hbl tempHbl = new Hbl();
+
+                if(i == 0) { //첫 기준 문자
+                    tempHbl.setHblYear(form.getBlYy());
+                    tempHbl.setHblNo(checkSplit[i]);
+                } else { //추가적인 번호가 있으면
+                    int cuttingLength = checkSplit[i].length();
+                    System.out.println(cuttingLength + " : 자를 길이");
+                    System.out.println(checkSplit[0] + " : 0번째 문자열, 기준문자열");
+                    checkSplit[i] = checkSplit[0].substring(0, checkSplit[0].length()-cuttingLength) + checkSplit[i];
+                    System.out.println(checkSplit[0].substring(0, checkSplit[0].length()-cuttingLength) + " : 자른후0번째 문자열");
+                    System.out.println(checkSplit[i]+ " : 수정된 문자열");
+                    tempHbl.setHblYear(form.getBlYy());
+                    tempHbl.setHblNo(checkSplit[i]);
+                }
+
+                hblList.add(tempHbl);
+            }
+        } else { //구분문자가 없을때 그냥 추가
+            Hbl tempHbl = new Hbl();
+            tempHbl.setHblYear(form.getBlYy());
+            tempHbl.setHblNo(checkSplit[0]);
+
+            hblList.add(tempHbl);
+        }
+
+        return hblList;
     }
 }
