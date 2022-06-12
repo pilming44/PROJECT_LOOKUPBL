509b43734d8358f72656029a1f0f6e2964000307
pilming44 <pilming44@naver.com>
2022-06-12 20:27:36 +0900

    부트스트랩 적용, 값 전달 이슈 - b/l번호 여러개 입력시 리스트로 값받게하는법 찾는중

--- src/main/java/jh/project/lookupbl/controller/MainController.java
+++ src/main/java/jh/project/lookupbl/controller/MainController.java
@@ -1,7 +1,8 @@
 package jh.project.lookupbl.controller;
 
+import jh.project.lookupbl.form.HblForm;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
@@ -9,5 +10,20 @@ public class MainController {
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/test")
+    public String test(@ModelAttribute HblForm hblForm) {
+        return "test";
+    }
+
+    @PostMapping("/test")
+    public String testPost(@ModelAttribute HblForm hblForm) {
+        //여러개 입력 시 구분자(,) 로 구분돼서 담아짐
+        System.out.println("---------------------> " + hblForm.getBlYy());
+        System.out.println("---------------------> " + hblForm.getHblNo());
+
+
+        return "test";
     }
 }
